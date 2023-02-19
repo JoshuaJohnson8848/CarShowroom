0bb037a5e2bbf6f409089a454aa86559c5b0b40a
Joshua Johnson <joshuatjohnson255@gmail.com>
2023-02-19 10:52:16 +0530

    Password Hashing Included

--- controller/admin.js
+++ controller/admin.js
@@ -1,4 +1,5 @@
 const Admin = require('../models/admin');
+const bcrypt = require('bcryptjs');
 
 exports.signup = async (req, res, next) => {
   try {
@@ -6,10 +7,24 @@ exports.signup = async (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
 
+    const existingEmail = await Admin.findOne({ email: email });
+    if (existingEmail) {
+      const error = new Error('Email Already Exist');
+      error.status = 422;
+      throw error;
+    }
+
+    const hashedPass = await bcrypt.hash(password, 12);
+    if (!hashedPass) {
+      const error = new Error('Password Error');
+      error.status = 422;
+      throw error;
+    }
+
     const admin = await new Admin({
       name: name,
       email: email,
-      password: password,
+      password: hashedPass,
     });
     const createdAdmin = await admin.save();
     if (!createdAdmin) {
